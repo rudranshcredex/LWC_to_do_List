d55188776117c9704b315c103a9da5783635ae2a
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2023-11-15 12:37:56 +0530

    tasks getting added to the list

--- force-app/main/default/lwc/todo/todo.js
+++ force-app/main/default/lwc/todo/todo.js
@@ -1,5 +1,31 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+
 
 export default class Todo extends LightningElement {
+    @track
+    userInput='';
+    task = [];
+
+    
+
+    handleInput(event){
+        this.userInput=event.target.value;
+        //console.log('this.userInput------>', this.userInput);
+    }
+    addTaskButton(){
+        this.task.push(this.userInput);
+        console.log('userinput----->', this.userInput);
+        console.log('this.task-------->', this.task);
+
+        
+        this.userInput = '';
+    }
+
+
+
+
+
+
+
     
 }
