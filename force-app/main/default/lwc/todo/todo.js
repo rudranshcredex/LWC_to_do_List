b30486291c34dc7e984f49b7db0abfba3e912b62
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2023-11-15 18:13:53 +0530

    strike through issues

--- force-app/main/default/lwc/todo/todo.js
+++ force-app/main/default/lwc/todo/todo.js
@@ -1,31 +1,40 @@
-import { LightningElement, wire, track } from 'lwc';
 
+import { LightningElement, track } from 'lwc';
 
 export default class Todo extends LightningElement {
-    @track
-    userInput='';
+    @track userInput = '';
     task = [];
+    taskList=[];
+    selectedId = [];
+    selected = true;
 
-    
-
-    handleInput(event){
-        this.userInput=event.target.value;
-        //console.log('this.userInput------>', this.userInput);
+    handleInput(event) {
+        this.userInput = event.target.value;
     }
-    addTaskButton(){
-        this.task.push(this.userInput);
-        console.log('userinput----->', this.userInput);
-        console.log('this.task-------->', this.task);
 
-        
+    addTaskButton() {
+        this.task.push({ id: this.task.length + 1, taskName: this.userInput, isSelected: false });
+        this.taskList = [...this.task];
         this.userInput = '';
     }
 
+    handleCheckboxChange(event) {
+        const taskId = event.target.value;
+        console.log('taskId------>', taskId);
 
+        this.selectedId.push(taskId);
+        // this.task.forEach(taskItem => {
+        //     if (taskItem.id === taskId) {
+        //         taskItem.isSelected = event.target.checked;
+        //     }
+        // });
 
+        console.log(' this.selectedId------->',  this.selectedId.length);
 
+    }
 
+    removeTasks(){
+        this.selected=false;
+    }
 
-
-    
 }
